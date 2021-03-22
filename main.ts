b1867ea57add51dcf87114d4317ab3c82d70decf
Vijay Cheleenahalli <cnvijayrao@gmail.com>
2021-03-21 21:38:25 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (text_list[randint(0, text_list.length - 1)]))
+})
+let text_list: string[] = []
+text_list = ["VIJAY", "SATISH", "BARGHAV", "RIDHAAN", "SANKETH"]
