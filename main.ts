50cc645ccffcc7109699931b1660517b7bc651d0
Vijay Cheleenahalli <cnvijayrao@gmail.com>
2021-03-21 21:39:30 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+/**
+ * This is a random name generator. picks a random name from a list
+ */
 input.onButtonPressed(Button.A, function () {
     basic.showString("" + (text_list[randint(0, text_list.length - 1)]))
 })
